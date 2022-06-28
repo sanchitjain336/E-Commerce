87a62db56f051398e7b74ba61b603442035292f6
Sanchit Jain <sanchitjain336@gmail.com>
2022-06-28 18:16:14 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import React from "react";
+// import toast, { Toaster } from "react-hot-toast";
+import { ToastContainer } from "react-toastify";
+import { Layout } from "../components";
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
+
+import { ToastProvider, useToasts } from "react-toast-notifications";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        {/* <ToastProvider> */}
+        <Component {...pageProps} />
+        {/* </ToastProvider> */}
+      </Layout>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
